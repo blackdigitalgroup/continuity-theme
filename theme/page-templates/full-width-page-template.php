26a7c3c9e8c0333fe4ee3c2a1294458211c90b62
blackdigitalgroup <111018807+blackdigitalgroup@users.noreply.github.com>
2022-08-23 06:57:07 -0500

    Initial commit

---
+++ theme/page-templates/full-width-page-template.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Continuity_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
